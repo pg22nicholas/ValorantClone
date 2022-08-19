9feb201d16f5e316e080e2b6742b49582258a715
pg22nicholas <pg22nicholas@vfs.com>
2022-08-19 01:24:27 -0700

    Added spawning logic for possessing a specific character type

--- Source/ValorantClone/Private/Player/ValorantPlayerControllerBase.cpp
+++ Source/ValorantClone/Private/Player/ValorantPlayerControllerBase.cpp
@@ -3,3 +3,35 @@
 
 #include "player/ValorantPlayerControllerBase.h"
 
+#include "Kismet/GameplayStatics.h"
+#include "Player/ValorantPlayerBase.h"
+#include "player/ValorantPlayerStateBase.h"
+
+void AValorantPlayerControllerBase::SER_SpawnPlayer_Implementation()
+{
+	UWorld* world = GetWorld();
+	if (!world) return;
+
+	AValorantCloneGameModeBase* GameMode = Cast<AValorantCloneGameModeBase>(UGameplayStatics::GetGameMode(world));
+	if (!GameMode) return;
+
+	AValorantPlayerStateBase* MyPlayerState = Cast<AValorantPlayerStateBase>(PlayerState);
+	if (!PlayerState) return;
+	
+	TSubclassOf<AValorantPlayerBase> PlayerCharacter = GameMode->GetPlayerCharacterType(MyPlayerState->GetPlayerType());
+
+	FActorSpawnParameters spawnParams;
+	spawnParams.Owner = this;
+	spawnParams.SpawnCollisionHandlingOverride = ESpawnActorCollisionHandlingMethod::AdjustIfPossibleButAlwaysSpawn;
+	
+	FTransform spawnTransform = GameMode->GetSpawnPoint(TEAMS::TEAM_A);
+	OwningCharacter = Cast<AValorantPlayerBase>(world->SpawnActor(PlayerCharacter, &spawnTransform, spawnParams));
+	if (OwningCharacter)
+	{
+		APawn* CachedPawn = GetPawn();
+		Possess(OwningCharacter);
+		
+		if (CachedPawn != nullptr)
+			CachedPawn->Destroy();
+	}
+}
--- Source/ValorantClone/Public/Player/ValorantPlayerStateBase.h
+++ Source/ValorantClone/Public/Player/ValorantPlayerStateBase.h
@@ -4,6 +4,7 @@
 
 #include "CoreMinimal.h"
 #include "GameFramework/PlayerState.h"
+#include "ValorantClone/ValorantCloneGameModeBase.h"
 #include "ValorantPlayerStateBase.generated.h"
 
 /**
@@ -13,5 +14,21 @@ UCLASS()
 class VALORANTCLONE_API AValorantPlayerStateBase : public APlayerState
 {
 	GENERATED_BODY()
+
+public:
+
+	AValorantPlayerStateBase();
+	
+	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly)
+	float Health = 100;
+
+	void SetPlayerType(PLAYABLE_CHARACTERS playerType) { PlayerType = playerType; }
+	PLAYABLE_CHARACTERS GetPlayerType() { return PlayerType; }
+
+private:
+	float CurrHealth;
+
+	// Type of player class this player state represents
+	PLAYABLE_CHARACTERS PlayerType;
 	
 };
