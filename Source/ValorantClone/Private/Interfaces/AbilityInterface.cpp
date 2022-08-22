5bcd890141b5f270bbc4367b023a61fabb1f281d
pg22nicholas <pg22nicholas@vfs.com>
2022-08-22 10:48:32 -0700

    Created empty AbilityInterface and SkillManager

---
+++ Source/ValorantClone/Private/Interfaces/AbilityInterface.cpp
@@ -0,0 +1,6 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "Interfaces/AbilityInterface.h"
+
+// Add default functionality here for any IAbilityInterface functions that are not pure virtual.
