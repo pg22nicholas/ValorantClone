dcad6701d1be485fe4e1ab9237e9c26f9669d0c8
Rafael Gazetti Bonfim <pg22rafael@vfs.com>
2022-08-24 12:04:20 -0700

    Creating new ability that creates a wall that blocks movement, it disappears after X seconds

---
+++ Source/ValorantClone/Private/Ability/BasicWallCreationAbility.cpp
@@ -0,0 +1,56 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "Ability/BasicWallCreationAbility.h"
+
+#include "Engine/StaticMeshActor.h"
+#include "Kismet/GameplayStatics.h"
+#include "Kismet/KismetMathLibrary.h"
+#include "Kismet/KismetSystemLibrary.h"
+
+bool ABasicWallCreationAbility::BeforeAbility(APlayerController* controller)
+{
+	if (!Super::BeforeAbility(controller)) return false;
+	UWorld* world = GetWorld();
+	if (!world) return false;
+	
+	return true;
+}
+
+bool ABasicWallCreationAbility::PerformAbility()
+{
+	if (!Super::PerformAbility()) return false;
+	
+	UWorld* world = GetWorld();
+	if (!world) return false;
+
+	if (!CachedPlayerController.IsValid()) return false;
+
+	// get player camera's rotation and position to line trace
+	FVector ViewportLocation;
+	FRotator ViewportRotation;
+	CachedPlayerController->GetPlayerViewPoint(OUT ViewportLocation, OUT ViewportRotation);
+	FVector EndCast = ViewportLocation + ViewportRotation.Vector() * 10000;
+
+
+	FHitResult HitResult;
+	if (UKismetSystemLibrary::LineTraceSingleForObjects(world, ViewportLocation, EndCast, LineCastType,
+		false, TArray<AActor*>(), EDrawDebugTrace::ForDuration, HitResult, true))
+	{
+		FVector HitLocation = HitResult.Location;
+		
+		// spawning the wall mesh where the player is aiming
+		AStaticMeshActor* NewWall = world->SpawnActor<AStaticMeshActor>(HitLocation, GetActorRotation());
+		NewWall->SetMobility(EComponentMobility::Movable);
+		NewWall->GetStaticMeshComponent()->SetStaticMesh(WallObject);
+		NewWall->GetStaticMeshComponent()->SetRelativeScale3D(ObjectScale);
+		NewWall->SetLifeSpan(WallDuration);
+	}
+	
+	return true;
+}
+
+bool ABasicWallCreationAbility::CancelAbility()
+{
+	return Super::CancelAbility();
+}
